d622ff9a53c4b17f41d4d9779468b2838e394fe7
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-10-24 18:42:19 +0200

    fix: make submit.py work properly

--- profiles/cluster/submit.py
+++ profiles/cluster/submit.py
@@ -14,8 +14,22 @@ cluster = job_properties["cluster"]
 
 conda_env = job_properties["cluster"]["conda_env"]
 
-with open(jobscript,'a') as f:
-    f.write(f"conda activate {conda_env};")
-    f.write("{exec_job}")
+print(jobscript)
+command = jobscript.splitlines()[-1]
 
-os.system(f"sbatch --time={cluster['time']} --mem={cluster['mem']} --cpus-per-task={cluster['n']} --qos={cluster['qos']} {jobscript}")
+template = f"""
+#!/bin/sh
+
+#SBATCH --output=log/%j.out                 # where to store the output ( %j is the JOBID )
+#SBATCH --error=log/%j.err                  # where to store error messages
+
+# activate conda environment
+source /scicore/home/neher/roemer0001/miniconda3/etc/profile.d/conda.sh
+conda activate {conda_env}
+export AUGUR_MINIFY_JSON=1
+export AUGUR_RECURSION_LIMIT=10000
+
+{command}
+"""
+
+os.system(f"sbatch --time={cluster['time']} --mem={cluster['mem']} --cpus-per-task={cluster['n']} --qos={cluster['qos']} {template}")
