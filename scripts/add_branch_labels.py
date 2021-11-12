dcaefd8b86cdaaf61a1ab0d637f5aa65412a116c
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-11-12 21:30:15 +0100

    feat: add insertions to special aa labels

--- scripts/add_branch_labels.py
+++ scripts/add_branch_labels.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 
+
 def extract_spike_mutations(node_data):
     data = {}
     for name, node in node_data["nodes"].items():
@@ -9,13 +10,15 @@ def extract_spike_mutations(node_data):
             data[name] = ", ".join(smuts)
     return data
 
+
 def extract_insertions(node_data):
     data = {}
     for name, node in node_data["nodes"].items():
         insertions = node["muts"]
         if insertions:
-            data[name] = ", ".join(insertions)
+            data[name] = insertions
     return data
+
 
 def extract_clade_labels(node_data):
     data = {}
@@ -24,16 +27,21 @@ def extract_clade_labels(node_data):
             data[name] = node["clade_annotation"]
     return data
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Remove extraneous colorings",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
-    parser.add_argument('--input', type=str, metavar="JSON", required=True, help="input Auspice JSON")
-    parser.add_argument('--mutations', type=str, required=False, help="mutations node data file")
-    parser.add_argument('--insertions', type=str, required=False, help="insertions node data file")
-    parser.add_argument('--output', type=str, metavar="JSON", required=True, help="output Auspice JSON")
+    parser.add_argument(
+        "--input", type=str, metavar="JSON", required=True, help="input Auspice JSON"
+    )
+    parser.add_argument("--mutations", type=str, required=False, help="mutations node data file")
+    parser.add_argument("--insertions", type=str, required=False, help="insertions node data file")
+    parser.add_argument(
+        "--output", type=str, metavar="JSON", required=True, help="output Auspice JSON"
+    )
     args = parser.parse_args()
 
     with open(args.input, "r") as f:
@@ -51,22 +59,34 @@ if __name__ == '__main__':
     else:
         insertions = {}
 
-    def attach_labels(n): # closure
+    def attach_labels(n):  # closure
         if n["name"] in spike_mutations:
             if "branch_attrs" not in n:
-                n["branch_attrs"]={}
+                n["branch_attrs"] = {}
             if "labels" not in n["branch_attrs"]:
-                n["branch_attrs"]["labels"]={}
+                n["branch_attrs"]["labels"] = {}
             n["branch_attrs"]["labels"]["spike_mutations"] = spike_mutations[n["name"]]
 
         if n["name"] in insertions:
             if "branch_attrs" not in n:
-                n["branch_attrs"]={}
+                n["branch_attrs"] = {}
             if "labels" not in n["branch_attrs"]:
-                n["branch_attrs"]["labels"]={}
-            n["branch_attrs"]["labels"]["insertions"] = insertions[n["name"]]
+                n["branch_attrs"]["labels"] = {}
+            if "aa" in n["branch_attrs"]["labels"]:
+                n["branch_attrs"]["labels"]["aa"] += "; "
+            else:
+                n["branch_attrs"]["labels"]["aa"] = ""
+            insertion_string = "; ".join(
+                [
+                    f'{insertion_type}: {", ".join(insertions[n["name"]][insertion_type])}'
+                    for insertion_type in insertions[n["name"]]
+                ]
+            )
+            n["branch_attrs"]["labels"]["aa"] += insertion_string
+
+            n["branch_attrs"]["labels"]["insertions"] = insertion_string
             if n["name"].startswith("NODE_"):
-                n["branch_attrs"]["labels"]["insertions (internal)"] = insertions[n["name"]]
+                n["branch_attrs"]["labels"]["insertions (internal)"] = insertion_string
 
         if "children" in n:
             for c in n["children"]:
@@ -74,5 +94,5 @@ if __name__ == '__main__':
 
     attach_labels(auspice_json["tree"])
 
-    with open(args.output, 'w') as f:
+    with open(args.output, "w") as f:
         json.dump(auspice_json, f, indent=2)
